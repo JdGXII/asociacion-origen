ef5aae8480b77c630e6c66a3e5f532c3e785c15f
Jose Alzaibar <jralzaibar@yahoo.com>
2020-04-24 15:15:04 -0500

    initial commit

---
+++ config.rb
@@ -0,0 +1,9 @@
+
+css_dir = "assets/css" # by Compass.app 
+sass_dir = "assets/sass" # by Compass.app 
+images_dir = "assets/img" # by Compass.app 
+output_style = :expanded # by Compass.app 
+relative_assets = true # by Compass.app 
+line_comments = true # by Compass.app 
+sass_options = {:debug_info=>true} # by Compass.app 
+sourcemap = true # by Compass.app 
